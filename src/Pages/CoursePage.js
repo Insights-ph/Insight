9a480a06294b6d9c570b84caf705273217939454
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2020-11-28 16:20:54 +0800

    📖Done: Course Page

--- src/Pages/CoursePage.js
+++ src/Pages/CoursePage.js
@@ -1,11 +1,34 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { InlineIcon } from "@iconify/react";
+import downOutlined from "@iconify-icons/ant-design/down-outlined";
 
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
-import { Container } from "react-bootstrap";
+import { Accordion, Card, Container } from "react-bootstrap";
 
 import { cardData } from "../MockData/Courses";
+
+const lessons = [
+  {
+    key: 1,
+    title: "Laboratory Fundamentals",
+    content:
+      "lorem ipsum dolor sit amet asd asd asd qwe qwe qwe asd asd placeholder text here asd asd asd qwe qwe qwe ",
+  },
+  {
+    key: 2,
+    title: "Common Laboratory Apparatuses",
+    content:
+      "lorem ipsum dolor sit amet asd asd asd qwe qwe qwe asd asd placeholder text here asd asd asd qwe qwe qwe ",
+  },
+  {
+    key: 3,
+    title: "Advanced Laboratory Apparatuses",
+    content:
+      "lorem ipsum dolor sit amet asd asd asd qwe qwe qwe asd asd placeholder text here asd asd asd qwe qwe qwe ",
+  },
+];
 
 export default function CoursePage() {
   const { courseId } = useParams();
@@ -15,6 +38,21 @@ export default function CoursePage() {
   )[0];
 
   console.log(selectedCourse);
+
+  let lessonAccordion = lessons.map((lesson) => (
+    <Card>
+      <Accordion.Toggle
+        as={Card.Header}
+        eventKey={lesson.key}
+        className="text-left bg-theme-accent-light d-flex align-items-center justify-content-between font-weight-bold"
+      >
+        {lesson.title} <InlineIcon icon={downOutlined} />
+      </Accordion.Toggle>
+      <Accordion.Collapse eventKey={lesson.key} className="text-justify">
+        <Card.Body>{lesson.content}</Card.Body>
+      </Accordion.Collapse>
+    </Card>
+  ));
 
   return (
     <Container fluid="md" className="position-relative mx-auto p-0">
@@ -27,8 +65,30 @@ export default function CoursePage() {
           paddingBottom: "75px",
         }}
       >
-        <h1>{selectedCourse.title}</h1>
-        <p>{selectedCourse.description}</p>
+        <Card
+          className="bg-dark text-white"
+          style={{ borderRadius: 0, outline: "none" }}
+        >
+          <Card.Img
+            src={selectedCourse.imgUrl}
+            alt={selectedCourse.title}
+            style={{ borderRadius: 0, filter: "brightness(65%)" }}
+          />
+          <Card.ImgOverlay className="d-flex flex-column h-100 align-items-start justify-content-center text-left">
+            <h4 >{selectedCourse.title.toUpperCase()}</h4>
+            <p className="m-0">{selectedCourse.name}</p>
+          </Card.ImgOverlay>
+        </Card>
+        <Container fluid="md" className="m-0 p-3 text-justify">
+          <h5 className="text-left font-weight-bold">About Course</h5>
+          <hr />
+          <p>{selectedCourse.description}</p>
+          <hr />
+        </Container>
+        <Container fluid="md" className="m-0 pl-3 pr-3 pt-0 pb-3 text-justify">
+          <h5 className="text-left font-weight-bold">Lessons</h5>
+        </Container>
+        <Accordion defaultActiveKey="0">{lessonAccordion}</Accordion>
       </div>
       <Footer />
     </Container>
